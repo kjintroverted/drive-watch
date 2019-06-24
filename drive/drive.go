f6f924e2220980b083766b60fc3efce45623eaec
Williamrobert Green <wkgreen13@gmail.com>
2019-06-24 15:51:12 -0400

    adds frontmatter to posts"

--- drive/drive.go
+++ drive/drive.go
@@ -1,15 +1,22 @@
 package drive
 
 import (
-	"fmt"
-	"strings"
-	"os"
-	"io"
 	"encoding/json"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
+	"time"
+
 	md "github.com/kjintroverted/html-to-markdown"
 )
+
+type fileInfo struct {
+	Name    string
+	ModTime time.Time
+}
 
 func AllDocToHTML(input, outDir string) {
 	files, _ := ioutil.ReadDir(input)
@@ -22,14 +29,22 @@ func DocToHTML(path, name, outDir string) {
 	if strings.Index(name, ".gdoc") < 0 {
 		return
 	}
+
+	fileName := strings.ReplaceAll(kebab(name), ".gdoc", "")
+
+	// SAVE METADATA AS JSON
+	info, _ := os.Stat(path + "/" + name)
+	jsonbytes, _ := json.Marshal(fileInfo{Name: strings.ReplaceAll(info.Name(), ".gdoc", ""), ModTime: info.ModTime()})
+	ioutil.WriteFile(outDir+"/"+fileName+".json", jsonbytes, 0644)
+
+	// SAVE CONTENT AS HTML
 	content, _ := ioutil.ReadFile(path + "/" + name)
 	var doc map[string]string
 	json.Unmarshal(content, &doc)
 	response, _ := http.Get("https://docs.google.com/feeds/download/documents/export/Export?exportFormat=html&id=" + doc["doc_id"])
-	fileName := strings.ReplaceAll(kebab(name), "gdoc", "html")
-	newFile, _ := os.Create(outDir + "/" + fileName)
+	newFile, _ := os.Create(outDir + "/" + fileName + ".html")
 	io.Copy(newFile, response.Body)
-	os.Chmod(outDir+"/"+fileName, 0644)
+	os.Chmod(outDir+"/"+fileName+".html", 0644)
 }
 
 func AllHTMLtoMD(input, outDir string) {
@@ -44,14 +59,27 @@ func HTMLtoMD(path, name, outDir string) {
 		return
 	}
 
-	raw, _ := ioutil.ReadFile(path+"/"+name)
-	
+	fileName := strings.ReplaceAll(kebab(name), ".html", "")
+
+	// CONVERT HTML
 	converter := md.NewConverter("", true, nil)
+	raw, _ := ioutil.ReadFile(path + "/" + name)
 	mdContent, _ := converter.ConvertString(string(raw))
+
+	// ADD FRONT MATTER
+	fileDataRaw, _ := ioutil.ReadFile(path + "/" + fileName + ".json")
+	var fileData fileInfo
+	json.Unmarshal(fileDataRaw, &fileData)
+	mdContent = fmt.Sprintf(`---
+title: %s
+lastUpdated: %s
+---
+%s`, fileData.Name, fileData.ModTime, mdContent)
+
 	err := ioutil.WriteFile(
-		outDir+"/"+strings.ReplaceAll(kebab(name), "html", "md"), 
-		[]byte(mdContent), 
-		0644) 
+		outDir+"/"+fileName+".md",
+		[]byte(mdContent),
+		0644)
 	if err != nil {
 		fmt.Println("ERROR", err)
 	}
